d6b6251e35824ff22f19f2a01b5c4b1cef024617
afifahrahmak <afifah.ark@gmail.com>
2023-06-15 10:07:42 +0700

    update app.js

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
+app.get('/', (req, res) => res.send('hello world'))
 app.use(routes)
 
 app.listen(port, () => {
